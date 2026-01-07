1175119e0e1a6727e51d4f715fc1ce60d096b8e2
Akinori TANIGUCHI <aknr.taniguchi@gmail.com>
2026-01-08 07:26:01 +0900

    fix: Web環境用guard (紙吹雪エフェクトを無効化)

--- components/Celebration.tsx
+++ components/Celebration.tsx
@@ -1,11 +1,17 @@
 import React, { forwardRef } from 'react';
 import ConfettiCannon from 'react-native-confetti-cannon';
 
+import { Platform, View, Text } from 'react-native';
+
 const Celebration = forwardRef((props, ref: any) => {
+  if (Platform.OS === 'web') {
+    return null; // ConfettiCannon may cause issues on web
+  }
+
   return (
     <ConfettiCannon
       count={200}
-      origin={{x: -10, y: 0}}
+      origin={{ x: -10, y: 0 }}
       autoStart={false}
       ref={ref}
       fadeOut={true}
